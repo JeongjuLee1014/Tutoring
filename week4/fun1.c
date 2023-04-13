946bba37c3a6a687a777a22bf0b5ee22ccf1878e
JeongjuLee1014 <ryan20115@naver.com>
2023-04-13 21:47:51 +0900

    directory upload

---
+++ week4/fun1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("2 X 1 = 2");
+	printf("2 X 2 = 4");
+	printf("2 X 3 = 6");
+	printf("2 X 4 = 8");
+	printf("2 X 5 = 10");
+	printf("2 X 6 = 12");
+	printf("2 X 7 = 14");
+	printf("2 X 8 = 16");
+	printf("2 X 9 = 18");
+
+	printf("3 X 1 = 3");
+	printf("3 X 2 = 6");
+	printf("3 X 3 = 9");
+	printf("3 X 4 = 12");
+	printf("3 X 5 = 15");
+	printf("3 X 6 = 18");
+	printf("3 X 7 = 21");
+	printf("3 X 8 = 24");
+	printf("3 X 9 = 27");
+
+	printf("4 X 1 = 4");
+	printf("4 X 2 = 8");
+	printf("4 X 3 = 12");
+	printf("4 X 4 = 16");
+	printf("4 X 5 = 20");
+	printf("4 X 6 = 24");
+	printf("4 X 7 = 28");
+	printf("4 X 8 = 32");
+	printf("4 X 9 = 36");
+
+	return 0;
+}
